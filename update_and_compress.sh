fd2a7e689e943162acc360026c5992de91ffa651
name <me32233@gmail.com>
2024-06-24 19:24:50 +0300

    only delete if docker finished updating

--- update_and_compress.sh
+++ update_and_compress.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/sh
 
-
+docker-compose down
+docker-compose up vscsync 
 mkdir -p output
 cd output
 rm *
-docker-compose down
-docker-compose up vscsync 
 tar -cvjf - ../artifacts/* | split -d -a 3 -b 150M - "artifacts.tar.bz2.part"
